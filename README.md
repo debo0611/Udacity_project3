35cd07e4cc9313159efd753a681e8f47ecbce40c
Debojyoti <debojyoti.pramanick@gmail.com>
2018-02-24 20:32:28 +0530

    first commit for repo

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+The main tool file is python program "news_db.py" which needs to be executed using python 3 interpretor.
+
+# python3 news_db.py 
+
+Each of the database queries are implemented as separate functions. Each function creates a connection to the 'news' database and closes the connection at the end of the function.
+The calls to the respective functions are in the bottom of the script. 
+By default the script will run all 3 queries one after the other.
+To run an individual query, uncomment the function call (for that particular query) and comment the other 2 function calls.
---
+++ news_db.py
@@ -0,0 +1,104 @@
+"""Tool to answer questions on the news database."""
+
+import psycopg2
+
+# db = psycopg2.connect("dbname=news")
+# c = db.cursor()
+
+
+def popular_articles(n):
+    """
+    Show the most popular ('n' number of) posts.
+
+    int n: number of posts to show.
+    return: None
+    """
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+
+    c.execute("select title, count(*) as total "
+              "from articles join log "
+              "on log.path = '/article/' || articles.slug "
+              "group by title "
+              "order by total "
+              "desc limit {}".format(n))
+    all_posts = c.fetchall()
+    print("{} most popular articles based on number "
+          "of views: ".format(n))
+    for post, view in all_posts:
+        print('"{}" -- {} views'.format(post, view))
+    print("\n=================================\n")
+    db.close()
+
+
+def popular_authors():
+    """
+    Show the most popular authors.
+
+    Print the names of mos popular authors along with the
+    respective page views of their articles.
+
+    return: None
+    """
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+
+    c.execute("select name, sum(total) as views "
+              "from authors join "
+              "(select author, title, count(*) as total "
+              "from articles join log "
+              "on log.path = '/article/' "
+              "|| articles.slug "
+              "group by author, title order by "
+              "total desc) as subq "
+              "on authors.id=subq.author "
+              "group by name order by views desc;")
+    all_posts = c.fetchall()
+    print("Most popular authors based on number of views:")
+    for name, view in all_posts:
+        print('{} -- {} views'.format(name, view))
+    print("\n================================\n")
+    db.close()
+
+
+def request_errors(n):
+    """
+    Show dates when more than 'n' % requests were errors.
+
+    int n: Errors of percentage of total requests
+           above which the date should be printed.
+
+    return: None
+    """
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+
+    c.execute("select dt, request_total, "
+              "request_failure, "
+              "(request_failure::float * 100 / "
+              "request_total) "
+              "as perc from "
+              "( select time::date as dt, "
+              "count(*) as request_total, "
+              "count(case when status <> '200 OK' "
+              "then 1 else null end) as request_failure "
+              "from log group by dt) as subq")
+    req_stats = c.fetchall()
+    print("Days when more than {} % requests led to errors".format(n))
+    for stat in req_stats:
+        if stat[3] > n:
+            print("{} -- {}% errors".
+                  format(
+                      stat[0].strftime("%b %d, %Y"),
+                      stat[3]
+                      )
+                  )
+    print("\n=================================\n")
+    db.close()
+
+
+if __name__ == "__main__":
+    print("\n=================================\n")
+    popular_articles(3)
+    popular_authors()
+    request_errors(1)
